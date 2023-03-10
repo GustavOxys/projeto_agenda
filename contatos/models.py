6f4cf5afa346f466afcb05e962c5db2a0c019aec
Gustavo <gustavohzmdev@gmail.com>
2023-03-10 14:48:12 -0300

    criado models e admin

--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=30)
+    sobrenome = models.CharField(max_length=30, blank=True) 
+    email = models.CharField(max_length=30, blank=True)
+    profissao = models.CharField(max_length=30)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(max_length=50, blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.nome
